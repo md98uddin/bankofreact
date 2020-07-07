4aeb3f48a653a4e75155f8553634cabeeb6618b9
md uddin <mduddin1998@yahoo.com>
2020-07-07 12:29:53 -0400

    Updates

---
+++ precache-manifest.6d60fe665e0e651ceef113ff4c2a7368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1d752ef9a69bff04ce957483a78e9f",
+    "url": "/bankofreact/index.html"
+  },
+  {
+    "revision": "ed3d2590d550effce5ba",
+    "url": "/bankofreact/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c250b95c91e899d716a8",
+    "url": "/bankofreact/static/css/main.167d9b65.chunk.css"
+  },
+  {
+    "revision": "ed3d2590d550effce5ba",
+    "url": "/bankofreact/static/js/2.3f57a1d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bankofreact/static/js/2.3f57a1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c250b95c91e899d716a8",
+    "url": "/bankofreact/static/js/main.9ad9d128.chunk.js"
+  },
+  {
+    "revision": "c4352f44ccfae1de56de",
+    "url": "/bankofreact/static/js/runtime-main.21a54ef4.js"
+  }
+]);
